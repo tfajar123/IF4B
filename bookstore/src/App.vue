05610db6d92b088e713665a66cc66105034f7f73
tfajar123 <tfajar123123@gmail.com>
2023-07-14 19:30:51 +0700

    Bookstore Vue

---
+++ bookstore/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <c-header />
+
+    <c-side-bar />
+    
+    <v-content>
+      <v-slide-y-transition mode="out-in">
+        <router-view></router-view>
+      </v-slide-y-transition>
+    </v-content>
+
+    <c-footer />
+  </v-app>
+</template>
+
+<script>
+  /* eslint-disable */
+  import CHeader from '@/components/CHeader.vue'
+  import CFooter from '@/components/CFooter.vue'
+  import CSideBar from '@/components/CSideBar.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      CHeader,
+      CFooter,
+      CSideBar,
+    }
+  }
+
+
+</script>
+
+<style type="text/css">
+  .v-toolbar {
+    flex: 0 !important;
+  }
+  .v-application .py-3 {
+    text-align: center !important;
+  }
+  .v-card__text {
+    text-align: center !important;
+  }
+</style>
